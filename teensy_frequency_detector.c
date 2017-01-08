f1f74220d089ed4015a91126010a34a5b90c8a24
Blake Rodriguez <blake.rodriguez@icloud.com>
2017-01-07 22:24:22 -0600

    add usb serial debug

--- teensy_frequency_detector.c
+++ teensy_frequency_detector.c
@@ -1,14 +1,19 @@
 #include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdint.h>
 #include <util/delay.h>
+#include "usb_serial.h"
 #include "sampling.h"
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 #define CPU_16MHz	0x00
 #define CPU_125kHz	0x07
+#define HEX(n) (((n) < 10) ? ((n) + '0') : ((n) + 'A' - 10))
 
 int main(void)
 {
   uint16_t val;
+  char buf[4];
 
   CPU_PRESCALE(CPU_125kHz);
   _delay_ms(1);		// allow slow power supply startup
@@ -23,10 +28,24 @@ int main(void)
   // turn off all ports
   PORTB, PORTC, PORTD, PORTE = 0x00;
 
-  adc_start(ADC_MUX_PIN_F0, ADC_REF_POWER);
+  usb_init();
+  while (!usb_configured()) {
+    PORTC = 2;
+    _delay_ms(1000);
+  }
+  _delay_ms(1000);
 
+  adc_start(ADC_MUX_PIN_F0, ADC_REF_POWER);
+  usb_serial_putchar('a');
   while (1) {
     val = adc_read();
+
+    buf[0] = HEX((val >> 8) & 15);
+    buf[1] = HEX((val >> 4) & 15);
+    buf[2] = HEX(val & 15);
+    buf[3] = ' ';
+
+    usb_serial_write((unsigned char *)buf, 4);
 
     // mic is clipping
     PORTC = (val == 1023 || val == 0) ? 2 : 0;
